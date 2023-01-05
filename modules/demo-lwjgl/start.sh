0c5ca43ba74d99c75267d7cec96379280b039bf4
totetmatt <matthieu.totet@gmail.com>
2023-01-05 08:38:05 +0100

    update pom to have profile with macos aarch64

--- modules/demo-lwjgl/start.sh
+++ modules/demo-lwjgl/start.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-
-java -jar -Xmx2048m target/viz-engine-lwjgl-demo-1.0.0-SNAPSHOT.jar "$@"
+if  [[ "$OSTYPE" == "darwin"* ]]; then
+  # on Macos, lwjgl expect to be launched with -XstartOnFirstThread on jvm
+  if  [[ "$1" == "GLFW" ]]; then
+    java -XstartOnFirstThread -jar -Xmx2048m target/viz-engine-lwjgl-demo-1.0.0-SNAPSHOT.jar "$@"
+  else
+    java -Dsun.java2d.metal=true -Dsun.java2d.noddraw=true -Dsun.awt.noerasebackground=true -jar -Xmx2048m target/viz-engine-lwjgl-demo-1.0.0-SNAPSHOT.jar "$@"
+  fi
+else
+  java -jar -Xmx2048m target/viz-engine-lwjgl-demo-1.0.0-SNAPSHOT.jar "$@"
+fi
