53b627411adeed880891f86688e6340180fe98c5
totetmatt <matthieu.totet@gmail.com>
2023-01-05 13:22:35 +0100

    refactor start.sh

--- modules/demo-lwjgl/start.sh
+++ modules/demo-lwjgl/start.sh
@@ -3,12 +3,12 @@
 mode=${1:-awt}
 mode=$(echo $mode | tr '[:upper:]' '[:lower:]')
 echo $mode
+
+extra_jvm_opt=""
 if  [[ "$OSTYPE" == "darwin"* ]]; then
   if  [[ "$1" == "glfw" ]]; then
-    java -XstartOnFirstThread -jar -Xmx2048m target/viz-engine-lwjgl-demo-1.0.0-SNAPSHOT.jar "$@"
-  else
-    java -jar -Xmx2048m target/viz-engine-lwjgl-demo-1.0.0-SNAPSHOT.jar "$@"
+    extra_jvm_opt="-XstartOnFirstThread"
   fi
-else
-  java -jar -Xmx2048m target/viz-engine-lwjgl-demo-1.0.0-SNAPSHOT.jar "$@"
 fi
+
+java -jar $extra_jvm_opt -Xmx2048m target/viz-engine-lwjgl-demo-1.0.0-SNAPSHOT.jar "$@"
